abf1a9d6f123e7f103da35430a668336c74fe8b7
kunal12kumar <23cs3035@rgipt.ac.in>
2024-08-12 21:13:40 +0530

    my new changes

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-const http= require('http');
-const fs= require('fs');
-const server= express();
-server.get()
+const http=require('http');
+const fs=require('fs');
+const path=require('path');
+const express=require('express');
+const server=express();
+
+// now to printing data on the server side by reading it with the help of fs
+
+const datar=fs.readFileSync('data.txt','UTF-8');
+const abs=path.resolve('index.html');
+console.log(abs);
+
+server.get('/',(req,res)=>{
+    console.log("enterd into server successfully");
+    console.log(req.url)
+    switch(req.url){
+        case '/':
+            // res.send(datar)
+            res.send(abs)
+            break;
+        
+        case '/demo':
+            res.sendFile(path.join(__dirname, 'index.html')); // Use path.join to resolve the file path
+            break;
+    }
+    
+});
+
+
+
+server.listen(3030,()=>{
+    console.log("server started succesfully")
+});
